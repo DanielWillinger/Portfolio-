8ec66938aa2c7d253930be10de8a4bfbb493e64e
DanielWillinger <94573051+DanielWillinger@users.noreply.github.com>
2024-11-25 17:11:42 -0500

    Add files via upload

---
+++ Program4Main/Program4Main/Product.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    public class Product
+    {
+        string SupplierName;
+        string ProductName;
+        string ProductType;
+        string supplierName { get; set; }
+       string productName { get; set; }
+        private int productID;
+       public int ProductID { get { return productID; } 
+         set 
+         { 
+         if (ProductID > 100000 && ProductID < 1000000) { }
+         else
+         {
+             ProductID = 0;
+         }
+
+         }
+     }
+
+       string productType { get; set; }
+
+        private double productPrice;
+       public double ProductPrice
+        { get { return productPrice; }
+            set { 
+                 if (ProductPrice < 0)
+                {
+                    ProductPrice = 0;
+                }
+            }
+        }
+        private int aisle;
+
+       public int  Aisle { get { return aisle; } set 
+            {
+
+                if (Aisle < 1 && Aisle > 20)
+                {
+                    Aisle = 0;
+                }
+            } }
+       bool InStockStatus;
+
+        public Product(string SupplierName,
+        string ProductName,
+        int ProductID,
+        string ProductType,
+        double ProductPrice,
+        int Aisle)
+        {
+            
+
+            supplierName = SupplierName;
+            productName = ProductName;
+            productID = ProductID;
+            productType = ProductType;
+            aisle = Aisle;
+            productPrice = ProductPrice;
+        }
+
+        public void InStock() { InStockStatus = true; }
+        public void OutofStock() { InStockStatus = false; }
+
+        public  override string ToString()
+        {
+            return String.Format($"Supplier's Name: {supplierName}\n" +
+                $"Product Name: {productName}\n" +
+                $"Product ID: {ProductID}\n" +
+                $"Product Type: {productType}\n" +
+                $"Product Price: {ProductPrice}\n" +
+                $"Aisle: {Aisle}\n" +
+                $"In Stock: {InStockStatus}\n");
+        }
+
+    }
+}
---
+++ Program4Main/Program4Main/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("Lays","Dorito's", 798744, "Chip", 4.99, 8);
+            Product product2 = new Product("Pepsi-Co", "Dr.Pepper", 789512, "Beverage", 1.00, 1);
+            Product product3 = new Product("Coca-Cola", "Coke", 123456, "Beverage", 2.00, 19);
+            Product product4 = new Product("Krispy Kreme", "Kreme Filled", 654987, "Doughnut", 7.00, 18);
+            Product product5 = new Product("Kellogs", "Frosted Flakes", 654321, "Cereal", 6.00, 10);
+
+            product4.InStock();
+            product5.InStock();
+
+            Product[] products = { product1, product2, product3, product4, product5 };
+
+            WriteOut(products);
+            
+        }
+
+        static void WriteOut(Product[] Products) 
+        { 
+            foreach (Product p in Products) 
+            {
+                Console.WriteLine(p.ToString());
+            }
+
+           Console.ReadLine();
+        }
+        
+    }
+}
